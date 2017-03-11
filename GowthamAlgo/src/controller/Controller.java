5e65875624f4277df64640dac8531b41be9f2b44
WinHan <su.win8188@gmail.com>
2017-03-11 16:56:50 +0000

    creating gowthamAlgo test

---
+++ GowthamAlgo/src/controller/Controller.java
@@ -0,0 +1,13 @@
+package controller;
+
+import model.Generator;
+
+import java.util.List;
+
+/**
+ * Created by WinHan on 11/3/17.
+ */
+public interface Controller {
+
+    int calculate(List<Generator> generatorList);
+}
---
+++ GowthamAlgo/src/model/Generator.java
@@ -0,0 +1,19 @@
+package model;
+
+
+public class Generator {
+
+    private int number;
+
+    public Generator(int number){
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
